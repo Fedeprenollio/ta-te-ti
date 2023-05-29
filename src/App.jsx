7449ff44a1c1d737408c298a7b6ea8f15b195768
Federico-xaca <federicoxaca@gmail.com>
2023-05-29 15:47:15 -0300

    add 4 en raya

--- src/App.jsx
+++ src/App.jsx
@@ -1,92 +1,107 @@
-import { useEffect, useState } from 'react'
-import confetti from 'canvas-confetti'
-import './App.css'
+import { useEffect, useState } from "react";
+import confetti from "canvas-confetti";
+import "./App.css";
 
-import { checkWinner, checkEndGame } from './logic/board'
-import { WinnerModal } from './components/WinnerModal'
-import Square from './components/Square'
-import { TURNS } from './constants'
-import { saveGameToStorage, resetGameToStorage } from './logic/storage'
-import { Timer } from './components/Timer'
+import { checkWinner, checkEndGame } from "./logic/board";
+import { WinnerModal } from "./components/WinnerModal";
+import Square from "./components/Square";
+import { TURNS } from "./constants";
+import { saveGameToStorage, resetGameToStorage } from "./logic/storage";
+import { Timer } from "./components/Timer";
+import { useTimer } from "./hooks/useTimer";
+import { SelectMode } from "./components/SelectMode";
 
-function App () {
-  const [timer , setTimer ] = useState(8)
-  const [timeLeft, setTimeLeft] = useState(timer)
+function App() {
+  const [boardSize, setboardSize] = useState({
+    size: 42,
+    grid: 6 * 7,
+    classNameGrid: "board6x7",
+  });
+
   const [board, setBoard] = useState(() => {
-    const boardFromLocalStorage = window.localStorage.getItem('board')
-    return boardFromLocalStorage ? JSON.parse(boardFromLocalStorage) : Array(9).fill(null)
-  }
-  )
+    const boardFromLocalStorage = window.localStorage.getItem("board");
+    return boardFromLocalStorage
+      ? JSON.parse(boardFromLocalStorage)
+      : Array(42).fill(null);
+  });
 
   const [turn, setTurn] = useState(() => {
-    const turnFromLocalStorage = window.localStorage.getItem('turn')
-    return turnFromLocalStorage ?? TURNS.X
-  }
-  )
+    const turnFromLocalStorage = window.localStorage.getItem("turn");
+    return turnFromLocalStorage ?? TURNS.X;
+  });
   // null es que no hay ganador, false es que hay empate
-  const [winner, setWinner] = useState(null)
-
+  const [winner, setWinner] = useState(null);
+  const { timer, setTimer, timeLeft, setTimeLeft } = useTimer({
+    board,
+    setWinner,
+    winner,
+  });
   const updateBoard = (index) => {
-    if (board[index] || winner) return // si existe algo en el casillero que marcamos, RETURN
-    const newBoard = [...board]
-    newBoard[index] = turn
-    setBoard(newBoard)
+    if (boardSize.size === 42) {
+      const tablero = board;
+      const columna = index % 7;
 
-    const newTurn = (turn === TURNS.X) ? TURNS.O : TURNS.X
-    if(newTurn){
-      setTimeLeft(timer)
-    }
-    setTurn(newTurn)
-    // guardar partida:
-    saveGameToStorage({ board: newBoard, turn: newTurn })
+      for (let fila = 5; fila >= 0; fila--) {
+        const posicion = fila * 7 + columna;
 
-    // revisar si tenemos ganador:
-    const newWinner = checkWinner(newBoard)
-    if (newWinner) {
-      confetti()
-      setWinner(newWinner)
-    } else if (checkEndGame(newBoard)) {
-      setWinner(false)
+        if (tablero[posicion] === null) {
+          console.log(tablero[posicion]);
+          const newBoard = [...board];
+          newBoard[posicion] = turn;
+          setBoard(newBoard);
+          const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X;
+          if (newTurn) {
+            setTimeLeft(timer);
+          }
+          setTurn(newTurn);
+          // guardar partida:
+          saveGameToStorage({ board: newBoard, turn: newTurn });
+
+          // revisar si tenemos ganador:
+          const newWinner = checkWinner(newBoard, boardSize.size);
+          if (newWinner) {
+            confetti();
+            setWinner(newWinner);
+          } else if (checkEndGame(newBoard)) {
+            setWinner(false);
+          }
+          break;
+        }
+      }
+    } else {
+      if (board[index] || winner) return; // si existe algo en el casillero que marcamos, RETURN
+      const newBoard = [...board];
+      newBoard[index] = turn;
+      setBoard(newBoard);
+
+      const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X;
+      if (newTurn) {
+        setTimeLeft(timer);
+      }
+      setTurn(newTurn);
+      // guardar partida:
+      saveGameToStorage({ board: newBoard, turn: newTurn });
+
+      // revisar si tenemos ganador:
+      const newWinner = checkWinner(newBoard, boardSize.size);
+      if (newWinner) {
+        confetti();
+        setWinner(newWinner);
+      } else if (checkEndGame(newBoard)) {
+        setWinner(false);
+      }
     }
-  }
+  };
 
   const resetGame = () => {
-    setBoard(Array(9).fill(null))
+    setBoard(Array(boardSize.size).fill(null));
     // TODO: HACER Q EL SIGUIENTE ES EL QUE GANo
-    setTurn(TURNS.X)
-    setWinner(null)
-    resetGameToStorage()
-    setTimeLeft(timer)
-  }
+    setTurn(TURNS.X);
+    setWinner(null);
+    resetGameToStorage();
+    setTimeLeft(timer);
+  };
 
-
-// TIMER logica
-  useEffect(() => {
-    if(board.includes(TURNS.X) || board.includes(TURNS.O)){
-      if (TURNS.X && timeLeft === 0) {
-       // Lógica a ejecutar cuando se agota el tiempo
-        setWinner(TURNS.O)
-      }else if(TURNS.O && timeLeft === 0){
-        setWinner(TURNS.X)
-      }
-  
-      const timer = setTimeout(() => {
-        setTimeLeft((prevTime) => prevTime - 1);
-      }, 1000);
-
-      if(winner !== null){
-        clearTimeout(timer)
-      }
-
-      return () => clearTimeout(timer);
-    }
-
-  }, [timeLeft,board]);
-
-  useEffect(() => {
- setTimeLeft(timer)
-  }, [timer])
-  
   // const handleTimeout = () => {
   //   // Lógica a ejecutar cuando se agota el tiempo
   //   // Por ejemplo:
@@ -101,44 +116,102 @@ function App () {
   //   setTimeLeft(2); // Reiniciar el tiempo para el nuevo jugador
   // };
 
-const handleChange=(e)=>{
-  const values = e.target.value
-  const lastLetter = values[values.length - 1];
-  if("012345678".includes(lastLetter) ) {
-    // lastLetter.slice(0, -1)
-    updateBoard(lastLetter)
-  }
+  const handleChange = (e) => {
+    const keysEnabled = boardSize?.size === 9 ? "123456789qweasdzxcuiojklnm," : "12345678"
+    const values = e.target.value;
+    const lastLetter = values[values.length - 1];
+    if (keysEnabled.includes(lastLetter)) {
+      const key = e.target.value.toLowerCase();
+      let value;
 
-}
+      switch (lastLetter) {
+        case "1":
+        case "q":
+        case "u":
+          value = 1;
+          break;
+        case "2":
+        case "w":
+        case "i":
+          value = 2;
+          break;
+        case "3":
+        case "e":
+        case "o":
+          value = 3;
+          break;
+        case "4":
+        case "a":
+        case "j":
+          value = 4;
+          break;
+        case "5":
+        case "s":
+        case "k":
+          value = 5;
+          break;
+        case "6":
+        case "d":
+        case "l":
+          value = 6;
+          break;
+        case "7":
+        case "z":
+        case "n":
+          value = 7;
+          break;
+        case "8":
+        case "x":
+        case "m":
+          value = 8;
+          break;
+        case "9":
+        case "c":
+        case ",":
+          value = 9;
+          break;
+        default:
+          value = null;
+          break;
+      }
+      updateBoard(value - 1);
+    }
+
+    // ------
+  };
 
   return (
-    <main className='board'>
+    <main className="board">
       <h1>Ta-te-ti</h1>
-      <Timer  timeLeft={timeLeft} setTimer={setTimer}/>
+      <div className="container-setting">
+        <SelectMode setboardSize={setboardSize} setBoard={setBoard} />
+        <Timer timeLeft={timeLeft} setTimer={setTimer} />
+      </div>
       <button onClick={resetGame}>Resetear el juego</button>
-      <section className='game'>
+      <section className={`game ${boardSize.classNameGrid}`}>
         {board.map((_, index) => {
           return (
-            <Square key={index} index={index} updateBoard={updateBoard}>{board[index]}</Square>
-          )
+            <Square key={index} index={index} updateBoard={updateBoard} boardSize={boardSize}>
+              {board[index]}
+            </Square>
+          );
         })}
-
       </section>
 
       <section>
         <form onChange={handleChange}>
-          <input type="text" />
+          <label htmlFor="keys">Tus teclas:</label>
+          <input type="text" id="keys" autoFocus placeholder="1 2 3 a s d..." />
         </form>
       </section>
-      <section className='turn'>
+      <section className="turn">
         <Square isSelected={turn === TURNS.X}>{TURNS.X}</Square>
         <Square isSelected={turn === TURNS.O}>{TURNS.O}</Square>
-     
       </section>
 
-      <WinnerModal winner={winner} resetGame={resetGame}  />
+      <WinnerModal winner={winner} resetGame={resetGame} />
     </main>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -1,15 +1,107 @@
-
-const Square = ({ children, isSelected, updateBoard, index }) => {
-  const className = `square container-key ${isSelected ? 'is-selected' : ''}`
+const Square = ({ children, isSelected, updateBoard, index, boardSize }) => {
+  console.log("ES?" ,boardSize)
+  const className = `square container-key ${isSelected ? "is-selected" : ""}`;
   const handleClick = () => {
-    updateBoard(index)
-  }
-  return (
-    <div  onClick={handleClick} className={className}>
-      {children}
-      <span className="key">{index}</span>
-    </div>
-  )
-}
+    updateBoard(index);
+  };
+  // para el tateti
+  function KeyBoardKeyTateti() {
+    switch (index) {
+      case 0:
+        return (
+          <div>
+            <span className="key-l">q</span>
+            <span className="key">{index + 1}</span>
+            <span className="key-r">u</span>
+          </div>
+        );
 
-export default Square
+        break;
+
+      case 1:
+        return (
+          <div>
+            <span className="key-l">w</span>
+            <span className="key">{index + 1}</span>
+            <span className="key-r">i</span>
+          </div>
+        );
+
+      case 2:
+        return (
+          <div>
+            <span className="key-l">e</span>
+            <span className="key">{index + 1}</span>
+            <span className="key-r">o</span>
+          </div>
+        );
+      case 3:
+        return (
+          <div>
+            <span className="key-l">a</span>
+            <span className="key">{index + 1}</span>
+            <span className="key-r">j</span>
+          </div>
+        );
+      case 4:
+        return (
+          <div>
+            <span className="key-l">s</span>
+            <span className="key">{index + 1}</span>
+            <span className="key-r">k</span>
+          </div>
+        );
+      case 5:
+        return (
+          <div>
+            <span className="key-l">d</span>
+            <span className="key">{index + 1}</span>
+            <span className="key-r">u</span>
+          </div>
+        );
+      case 6:
+        return (
+          <div>
+            <span className="key-l">z</span>
+            <span className="key">{index + 1}</span>
+            <span className="key-r">n</span>
+          </div>
+        );
+      case 7:
+        return (
+          <div>
+            <span className="key-l">x</span>
+            <span className="key">{index + 1}</span>
+            <span className="key-r">m</span>
+          </div>
+        );
+      case 8:
+        return (
+          <div>
+            <span className="key-l">c</span>
+            <span className="key">{index + 1}</span>
+            <span className="key-r">,</span>
+          </div>
+        );
+      default:
+        break;
+    }
+  }
+
+  // para el 4 en linea
+  function KeyBoard4InLine() {
+    if (index <= 6) {
+      return <span className="key">{index + 1}</span>;
+    }
+  }
+
+  return (
+    <div onClick={handleClick} className={className}>
+      {children}
+      {/* <span className="key">{index + 1}</span> */}
+      {boardSize?.size === 9 ? <KeyBoardKeyTateti /> : <KeyBoard4InLine />}
+    </div>
+  );
+};
+
+export default Square;
